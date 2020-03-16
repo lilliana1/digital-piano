e8c59cb27096705518367c76c69cf93214d19c74
Lilliana Ramos <lilliana.cramos@gmail.com>
2020-03-16 15:29:34 -0400

    works is press keyboard on computer

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+// keyboard keys
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
+
+
 // calling all keys
 const keys = document.querySelectorAll('.key')
+const whiteKeys = document.querySelectorAll('.key.white')
+const blackKeys = document.querySelectorAll('.key.black')
 
 keys.forEach(key => {
     key.addEventListener('click', () => playNote(key))
+})
+
+// allow to play audio when computer keyboard is press
+document.addEventListener('keydown', e => {
+    // when key is press it wont sound horrible, it will sound good
+    if (e.repeat) return
+    const key = e.key
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+    const blackKeyIndex = BLACK_KEYS.indexOf(key)
+
+    if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex]);
+    if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex]);
 })
 
 function playNote(key) {
