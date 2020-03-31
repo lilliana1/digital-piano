f88c5cddcf73389afdcb47d222d5359cf940e6d7
Lilliana Ramos <lilliana.cramos@gmail.com>
2020-03-30 23:26:35 -0400

    setting up mongodb

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Song = require('./models/song.js')
 const app = express()
 
-app.set('view engine', 'ejs')
+mongoose.connect('mongodb://localhost.songRecorder', {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
 
+app.set('view engine', 'ejs')
+app.use(express.json())
 // telling express where the files are
 app.use(express.static('public'))
 
 // index route
 app.get('/', (req, res) => {
     res.render('index')
+})
+
+app.post('/songs', (req, res) => {
+    req.body.songNotes
 })
 
 app.listen(5000)
